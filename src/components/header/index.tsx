056927597395bd0c31049729b22141f69bcec7a6
Juliano Ventola <ventolajuliano@gmail.com>
2025-09-02 20:41:56 -0300

    feat: adds header component details

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,24 @@
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router";
+
 function Header() {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <header className="w-full px-1 bg-slate-200 flex">
+      <nav className="w-full max-w-7xl h-14 flex items-center justify-between px-5 m-auto">
+        <Link className="font-bold text-2xl" to="/">Shopping</Link>
+
+
+        <Link to="/cart" className="relative">
+         <FiShoppingCart size={24} color="#121212"/>
+         <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full
+          w-6 h-6 flex justify-center items-center
+          text-xs text-white
+          ">
+          2
+         </span>
+        </Link>
+      </nav>
+    </header>
   );
 }
 
